b3c3b5eb4aa81e75d6a25cc1069293b7eb45a45f
caio-martins_frtlx <caio.martins@foretellix.com>
2025-03-12 21:46:26 -0300

    Add final praticial 1

--- pointwise/src/Main.java
+++ pointwise/src/Main.java
@@ -1,16 +1,66 @@
 import ij.IJ;
 import ij.ImagePlus;
+import ij.ImageStack;
+import ij.measure.ResultsTable;
+import ij.plugin.filter.ParticleAnalyzer;
 import ij.process.ImageProcessor;
 
 public class Main {
     public static void main(String[] args) {
-        ImagePlus image = IJ.openImage("./data/microtubules.tif", 1);
+        Main.q3();
+    }
+    public static void q3() {
+        String[] imageFiles = {
+                "./data/yeast1.tif",
+                "./data/yeast2.tif",
+                "./data/yeast3.tif",
+                "./data/yeast4.tif"
+        };
+
+        for (String file : imageFiles) {
+            ImagePlus imp = IJ.openImage(file);
+            if (imp == null) {
+                IJ.log("Não foi possível abrir a imagem: " + file);
+                continue;
+            }
+
+            imp.setTitle("Original - " + file);
+            imp.show();
+            
+            IJ.run(imp, "Make Binary", "");
+
+            ResultsTable rt = new ResultsTable();
+            int options = ParticleAnalyzer.SHOW_NONE;
+            int measurements = 0;
+            ParticleAnalyzer pa = new ParticleAnalyzer(options, measurements, rt, 0, Double.POSITIVE_INFINITY);
+
+            if (pa.analyze(imp)) {
+                int numParticles = rt.getCounter();
+                IJ.log("Imagem: " + file + " - Número de partículas detectadas: " + numParticles);
+            } else {
+                IJ.log("Falha na análise de partículas para a imagem: " + file);
+            }
+
+            rt.show("Resultados - " + file);
+        }
+    }
+
+    public static void q4(){
+        ImagePlus image = IJ.openImage("./data/yeast_stack.tif");
         if (image != null) {
-            ImageProcessor ip = image.getProcessor();
-            ImageAccess ass = new ImageAccess(ip);
-            PointwiseTransform.rescale(ass);
-            image.show("");
-            ass.show("");
+            ImageStack stack = image.getStack();
+            int nSlices = stack.getSize();
+            ImageAccess[] zstack = new ImageAccess[nSlices];
+            for (int i = 0; i < nSlices; i++) {
+                ImageProcessor ip = stack.getProcessor(i + 1);
+                zstack[i] = new ImageAccess(ip);
+            }
+
+            ImageAccess maxProjection = PointwiseTransform.zprojectMaximum(zstack);
+            maxProjection.show("Projeção Máxima");
+
+            ImageAccess meanProjection = PointwiseTransform.zprojectMean(zstack);
+            meanProjection.show("Projeção Média");
         }
     }
 }
--- pointwise/src/PointwiseTransform.java
+++ pointwise/src/PointwiseTransform.java
@@ -8,13 +8,14 @@ public class PointwiseTransform extends Object {
 		int ny = input.getHeight();
 		ImageAccess output = new ImageAccess(nx, ny);
 		double value = 0.0;
-		for (int x=0; x<nx; x++)
-		for (int y=0; y<ny; y++) {
-			value = input.getPixel(x, y);
-			value = 255 - value;
-			output.putPixel(x, y, value);
+		for (int x=0; x<nx; x++) {
+			for (int y = 0; y < ny; y++) {
+				value = input.getPixel(x, y);
+				value = 255 - value;
+				output.putPixel(x, y, value);
+			}
 		}
-		return output;	
+		return output;
 	}
 
 	/**
@@ -25,9 +26,12 @@ public class PointwiseTransform extends Object {
 		int ny = input.getHeight();
 		double max = input.getMaximum();
 		double min = input.getMinimum();
+
 		ImageAccess output = new ImageAccess(nx, ny);
+
 		double alpha = 255.0 / (max - min);
 		double beta = min;
+
 		for (int y = 0; y < ny; y++) {
 			for (int x = 0; x < nx; x++) {
 				double fxy = input.getPixel(x, y);
@@ -45,32 +49,64 @@ public class PointwiseTransform extends Object {
 		int nx = input.getWidth();
 		int ny = input.getHeight();
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
-		return output;
+
+		for (int y = 0; y < ny; y++) {
+			for (int x = 0; x < nx; x++) {
+				double value = input.getPixel(x, y);
+				if (value > 10000) {
+					output.putPixel(x, y, 10000);
+				} else {
+					output.putPixel(x, y, value);
+				}
+			}
+		}
+
+		return rescale(output);
 	}
-	
+
 	/**
-	* Question 4.1 Maximum Intensity Projection
-	*/
+	 * Question 4.1 Maximum Intensity Projection
+	 */
 	static public ImageAccess zprojectMaximum(ImageAccess[] zstack) {
 		int nx = zstack[0].getWidth();
 		int ny = zstack[0].getHeight();
 		int nz = zstack.length;
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
-		return output;	
+
+		for (int y = 0; y < ny; y++) {
+			for (int x = 0; x < nx; x++) {
+				double maxValue = Double.NEGATIVE_INFINITY;
+				for (int z = 0; z < nz; z++) {
+					double pixelValue = zstack[z].getPixel(x, y);
+					if (pixelValue > maxValue) {
+						maxValue = pixelValue;
+					}
+				}
+				output.putPixel(x, y, maxValue);
+			}
+		}
+		return output;
 	}
 
 	/**
-	* Question 4.2 Z-stack mean
-	*/
+	 * Question 4.2 Z-stack Mean
+	 */
 	static public ImageAccess zprojectMean(ImageAccess[] zstack) {
 		int nx = zstack[0].getWidth();
 		int ny = zstack[0].getHeight();
 		int nz = zstack.length;
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
-		return output;	
-	}
 
+		for (int y = 0; y < ny; y++) {
+			for (int x = 0; x < nx; x++) {
+				double sum = 0;
+				for (int z = 0; z < nz; z++) {
+					sum += zstack[z].getPixel(x, y);
+				}
+				double meanValue = sum / nz;
+				output.putPixel(x, y, meanValue);
+			}
+		}
+		return output;
+	}
 }
